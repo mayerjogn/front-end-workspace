4780944c6ea5b0d112f6ddba8158a471c39462c9
mayerjogn <130232731+mayerjogn@users.noreply.github.com>
2023-09-14 11:51:10 +0900

    React 8-Hooks
    useState

--- 7-jsx/src/App.js
+++ 7-jsx/src/App.js
@@ -1,24 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import{useState} from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+const App = () => {
+  const [counter, setCounter] = useState(0);
+
+  const setCount =()=>{
+  setCounter(counter + 1);
+};
+  
+  return(// 리턴 안에는 무조건 하나의 함수가 들어가야됨
+  <div>
+    <h1>Total clicks: {counter}</h1>
+    <button onClick={setCount}>Click!</button>
+  </div>
   );
 }
 
--- 8-hooks/src/index.js
+++ 8-hooks/src/index.js
@@ -3,9 +3,7 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 
 
-// index.js의 완전 기본 세팅
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render( 
-    <App />
-);
+root.render(<App />);
+
 
